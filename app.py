8d2f1fb328321b13a88dc4fc45d241c8f7f3b8de
levvid <glm83@cornell.edu>
2017-07-07 03:46:25 -0400

    tab bug

--- app.py
+++ app.py
@@ -10,32 +10,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def readPlay():
-	file = urllib2.urlopen(' http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
-	data = file.read()
-	file.close()
+    file = urllib2.urlopen(' http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
+    data = file.read()
+    file.close()
 
-	speakerDict = {}
-	sortedSpeakerDict = {}
-	listS = []
-	root = ET.fromstring(data)
-	#find all speeches
-	for speech in root.findall('./ACT/SCENE/SPEECH'): 
-		#find the speaker name
-		speaker = speech.find('SPEAKER').text 
-    	#skip if speaker name is ALL
-    	if speaker == "ALL": 
-    		continue 
-    	#convert speaker label to CamelCase for graph
-    	speaker = speaker.title() 
+    speakerDict = {}
+    sortedSpeakerDict = {}
+    listS = []
+    root = ET.fromstring(data)
+    #find all speeches
+    for speech in root.findall('./ACT/SCENE/SPEECH'): 
+        #find the speaker name
+        speaker = speech.find('SPEAKER').text 
+        #skip if speaker name is ALL
+        if speaker == "ALL": 
+            continue 
+        #convert speaker label to CamelCase for graph
+        speaker = speaker.title() 
         #find all the lines by this speaker as a list
         lineList = speech.findall('LINE') 
         numberOfLines = len(lineList) 
-        
-    	#if the speaker is already on the dict add to their number of lines
-    	if speaker in speakerDict:
-    		speakerDict[speaker] += numberOfLines 
-    	else:
-    		speakerDict[speaker] = numberOfLines 
+
+        #if the speaker is already on the dict add to their number of lines
+        if speaker in speakerDict:
+            speakerDict[speaker] += numberOfLines 
+        else:
+            speakerDict[speaker] = numberOfLines 
 
     #sort the speakers according to their number of lines in descending order
     sortedSpeakerDict = OrderedDict(sorted(speakerDict.items(), key=lambda x: -x[1]))
@@ -44,9 +44,9 @@ def readPlay():
     labels = list(sortedSpeakerDict.keys())
     values = list(sortedSpeakerDict.values())
     for i in range(len(labels)):
-    	print labels[i], values[i]
-    	return render_template('chart.html', values=values, labels=labels)
+        print labels[i], values[i]
+        return render_template('chart.html', values=values, labels=labels)
 
-    	if __name__ == "__main__":
-    		port = int(os.environ.get("PORT", 5000))
-    		app.run(host='0.0.0.0', port=port)
+        if __name__ == "__main__":
+            port = int(os.environ.get("PORT", 5000))
+            app.run(host='0.0.0.0', port=port)
